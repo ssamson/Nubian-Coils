5bfa3e19851fb284a7b73352779fce06029f7f6e
Stephanie Samson <sasamson98@hotmail.com>
2020-01-23 15:59:14 -0500

    updated sign in sign up

--- client/src/components/SignInSignUp.js
+++ client/src/components/SignInSignUp.js
@@ -1,53 +1,82 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 import "./SignInSignUp.css";
 
 export default function SignInSignUp() {
-  let container;
-  useEffect(() => {
-    container = document.getElementById("container");
-  }, []);
+  const container = useRef(null);
+
+  const [email, setEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [accountType, setAccountType] = useState("salon");
+  const history = useHistory();
+
+  const handleLoginSubmit = async e => {
+    e.preventDefault();
+    const loginData = { email, password: loginPassword };
+    const res = await axios.post("/api/user/login", loginData);
+    if (res.data) {
+      localStorage.setItem("token", res.data.token);
+      const user = res.data.user;
+      history.push(`/profile/${user._id}`);
+    } else {
+      alert("Invalid Credential");
+    }
+  };
+
+  const handleRegisterSubmit = async e => {
+    e.preventDefault();
+    const res = await axios.post("/api/user/", {
+      email,
+      password: registerPassword,
+      accountType
+    });
+    localStorage.setItem("token", res.data.token);
+    history.push(`/profile/${res.data.user._id}`);
+  };
 
   return (
-    <div className="container" id="container">
+    <div className="container sign-in-sign-up" ref={container}>
       <div className="form-container sign-up-container">
-        <form action="#">
+        <form onSubmit={handleRegisterSubmit}>
           <h1>Create Account</h1>
-          {/* <div className="social-container">
-            <a href="!#" className="social">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="!#" className="social">
-              <i className="fab fa-google-plus-g"></i>
-            </a>
-            <a href="!#" className="social">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-          </div> */}
-          {/* <span>or use your email for registration</span> */}
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <select
+            className="form-control"
+            onChange={e => setAccountType(e.target.value)}
+          >
+            <option value="salon">Salon/Stylist</option>
+            <option value="customer">Customer</option>
+          </select>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={registerPassword}
+            onChange={e => setRegisterPassword(e.target.value)}
+          />
           <button>Sign Up</button>
         </form>
       </div>
       <div className="form-container sign-in-container">
-        <form action="!#">
+        <form onSubmit={handleLoginSubmit}>
           <h1>Sign in</h1>
-          {/* <div className="social-container">
-            <a href="#!" className="social">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="!#" className="social">
-              <i className="fab fa-google-plus-g"></i>
-            </a>
-            <a href="!#" className="social">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-          </div> */}
-          {/* <span>or use your account</span> */}
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <a href="!#">Forgot your password?</a>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={e => setLoginPassword(e.target.value)}
+          />
           <button>Sign In</button>
         </form>
       </div>
@@ -62,8 +91,9 @@ export default function SignInSignUp() {
               className="ghost"
               id="signIn"
               onClick={() => {
-                container.classList.remove("right-panel-active");
+                container.current.classList.remove("right-panel-active");
               }}
+              type="button"
             >
               Sign In
             </button>
@@ -74,8 +104,9 @@ export default function SignInSignUp() {
             <button
               className="ghost"
               id="signUp"
+              type="button"
               onClick={() => {
-                container.classList.add("right-panel-active");
+                container.current.classList.add("right-panel-active");
               }}
             >
               Sign Up
