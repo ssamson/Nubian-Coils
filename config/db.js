90fa8853b33bced6380472ad4191fe2495832317
Stephanie Samson <sasamson98@hotmail.com>
2020-01-23 16:03:19 -0500

    updates

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const db = config.get("mongo");
+
+async function connectDB() {
+  if (process.env.NODE_ENV === "production") {
+    const username = process.env.MLAB_USERNAME;
+    const password = process.env.MLAB_PASSWORD;
+    let connection = `mongodb://${username}:${password}${db}`;
+    await mongoose.connect(connection, {
+      useNewURLParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+  } else {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+  }
+}
+
+module.exports = connectDB;
---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  displayName: { type: String, default: "Anonymous" },
+  salonName: { type: String, default: "" },
+  accountType: { type: String, required: true },
+  locationName: { type: String, default: "" },
+  joinDate: { type: Date, default: Date.now() },
+  favoriteHairStyle: { type: String, default: "" },
+  streetName: { type: String, default: "" },
+  cityName: { type: String, default: "" },
+  stateName: { type: String, default: "" },
+  zipCode: { type: String, default: "" },
+  phoneNumber: { type: Number, default: "" },
+  salonWebsite: { type: String, default: "" },
+  salonServices: { type: Array, default: "" },
+  salonAbout: { type: String, default: "" },
+  hairSalonPics: { type: String, default: "" }
+});
+
+module.exports = mongoose.model("User", UserSchema);
