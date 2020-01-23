0ccbbe66924416f6498d63206494694807100737
Stephanie Samson <sasamson98@hotmail.com>
2020-01-23 16:00:40 -0500

    updated routes

---
+++ routes/user.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcryptjs");
+
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+const User = require("../models/User");
+
+// Login (SignIn)
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  // check if username is correct
+  let user = await User.findOne({ email });
+  if (!user) {
+    res.json(null);
+  } else {
+    // Validate password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.json(null);
+    } else {
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: "1d"
+        },
+        (error, token) => {
+          if (error) {
+            throw error;
+          }
+          res.json({ token, user });
+        }
+      );
+    }
+  }
+});
+
+//Register(SignUp)
+router.post("/", async (req, res) => {
+  const newUser = new User({ ...req.body });
+
+  const user = await User.findOne({ email: req.body.email });
+
+  if (user) {
+    res.json(null);
+    return;
+  }
+  // Create salt & hash
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      throw err;
+    }
+    bcrypt.hash(newUser.password, salt, async (err, hash) => {
+      if (err) {
+        throw err;
+      }
+      newUser.password = hash;
+      const user = await newUser.save();
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: "1d" },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token, user });
+        }
+      );
+    });
+  });
+});
+
+// Find user by id
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await User.findById(id);
+  res.json(user);
+});
+
+module.exports = router;
