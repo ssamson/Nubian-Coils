95cebb2e0bdd07b2a85691f29e8dc4f3edc0f8c5
Stephanie Samson <sasamson98@hotmail.com>
2020-01-23 15:57:27 -0500

    updated header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,38 +3,36 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>
-      <nav className="navbar navbar-light navbar-expand-lg fixed-top">
-        <a href="#" className="navbar-brand">
-          NUBIAN COILS | Salon & Stylist Finder
-        </a>
-        <button
-          className="navbar-toggler"
-          data-toggle="collapse"
-          data-target="#navbarCollapse"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <ul className="navbar-nav ml-auto">
-            <li className="navbar-item">
-              <a href="#" className="nav-link">
-                HOME
-              </a>
-            </li>
-            <li className="navbar-item">
-              <Link to="/sign-in-sign-up" className="nav-link">
-                Sign in / Sign up
-              </Link>
-            </li>
-            {/* <li className="navbar-item">
+    <nav className="navbar navbar-light navbar-expand-lg fixed-top">
+      <a href="#" className="navbar-brand display-1">
+        NUBIAN COILS | Salon & Stylist Finder
+      </a>
+      <button
+        className="navbar-toggler"
+        data-toggle="collapse"
+        data-target="#navbarCollapse"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <ul className="navbar-nav ml-auto">
+          <li className="navbar-item">
+            <a href="/" className="nav-link">
+              HOME
+            </a>
+          </li>
+          <li className="navbar-item">
+            <Link to="/sign-in-sign-up" className="nav-link">
+              Sign in / Sign up
+            </Link>
+          </li>
+          {/* <li className="navbar-item">
               <a href="#" className="nav-link">
                 SIGN UP
               </a>
             </li> */}
-          </ul>
-        </div>
-      </nav>
-    </div>
+        </ul>
+      </div>
+    </nav>
   );
 }
